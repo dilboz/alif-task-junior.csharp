c2d4a7867b662a8dfc285aee87917aba510e2569
dilboz <dilbozsafarov2002@gmail.com>
2023-07-21 11:02:22 +0500

    Add Deafult data and Create first migration

--- Task.Api/Data/ApplicationDbContext.cs
+++ Task.Api/Data/ApplicationDbContext.cs
@@ -10,5 +10,18 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasIndex(c => c.CategoryName)
+            .IsUnique();
+
+        //add default data
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = Guid.NewGuid(), CategoryName = "smartphone", RangeFrom = 3, RangeTo = 9, Percent = 3 },
+            new Category { Id = Guid.NewGuid(), CategoryName = "computer", RangeFrom = 3, RangeTo = 12, Percent = 4 },
+            new Category { Id = Guid.NewGuid(), CategoryName = "tv", RangeFrom = 3, RangeTo = 18, Percent = 5}
+        );
+    }
 }
---
+++ Task.Api/Migrations/20230721055822_First-Migration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Task.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CategoryName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    RangeFrom = table.Column<int>(type: "integer", nullable: false),
+                    RangeTo = table.Column<int>(type: "integer", nullable: false),
+                    Percent = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CategoryName", "Percent", "RangeFrom", "RangeTo" },
+                values: new object[,]
+                {
+                    { new Guid("2ab6dbd8-da7d-40ad-86b4-8a002eb3994f"), "smartphone", 3, 3, 9 },
+                    { new Guid("68131fd5-5d02-4d7b-8768-57087a204e67"), "tv", 5, 3, 18 },
+                    { new Guid("f83f47d3-b583-4e6e-af03-116bf735a172"), "computer", 4, 3, 12 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryName",
+                table: "Categories",
+                column: "CategoryName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
