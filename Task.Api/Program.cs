3694f73a6622768e978d2da9c5e13acb73c4e2d9
dilboz <dilbozsafarov2002@gmail.com>
2023-07-21 10:45:14 +0500

    Add Swagger

--- Task.Api/Program.cs
+++ Task.Api/Program.cs
@@ -1,19 +1,37 @@
+using System.Reflection;
+using Api.Task.Services;
 using Microsoft.EntityFrameworkCore;
 using Task.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
 //Postgres-Connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionPostgres") ?? string.Empty));
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<CategoryService>();
+
+builder.Services.AddSwaggerGen(config =>
+{
+    var xmlFile = $"{Assembly.GetEntryAssembly()!.GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    config.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI(config =>
+{
+    config.RoutePrefix = string.Empty;
+    config.SwaggerEndpoint("swagger/v1/swagger.json", "Swagger Task-Junior-CSharp-Alif Api");
+});
 
 #pragma warning disable ASP0014
 app.UseEndpoints(endpoints => { endpoints?.MapControllers(); });
 #pragma warning restore ASP0014
+
 
 app.Run();
